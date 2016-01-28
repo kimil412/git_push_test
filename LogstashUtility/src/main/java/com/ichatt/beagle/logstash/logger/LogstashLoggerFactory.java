f0d929b228bd9964681febb011630b0699b4bc3b
kimilb <kimilb>
2016-01-28 16:26:22 +0900

    v0.9.89 general logger generating function added.

--- LogstashUtility/src/main/java/com/ichatt/beagle/logstash/logger/LogstashLoggerFactory.java
+++ LogstashUtility/src/main/java/com/ichatt/beagle/logstash/logger/LogstashLoggerFactory.java
@@ -4,19 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.StaticLoggerBinder;
 
-//import ch.qos.logback.classic.LoggerContext;
-//import ch.qos.logback.classic.util.ContextInitializer;
-
-//import org.apache.log4j.Logger;
-
 public class LogstashLoggerFactory {
 
-    private int connectPortNumber;
     private static LogstashLoggerFactory instance = null;
-
-    protected LogstashLoggerFactory() {
-        connectPortNumber = 2323;
-    }
     
     public static LogstashLoggerFactory getInstance() {
         if( instance == null ) {
@@ -26,28 +16,10 @@ public class LogstashLoggerFactory {
         return instance;
     }
 
-    public void setConnectPortNumber(int portNumber) {
-    	this.connectPortNumber = portNumber;
-    }
-    
-    public int getConnectPortNumber() {
-    	return this.connectPortNumber;
-    }
     public Logger getLogger() {
-    	//return Logger.getLogger(LogstashLoggerFactory.class);
-    	//System.out.println(LogstashLoggerFactory.class.getName());
     	return LoggerFactory.getLogger(LogstashLoggerFactory.class);
-    	//return Logger.getRootLogger();
     }
     
-   /* public LoggerContext getLoggerContext() {
-    	return (LoggerContext) LoggerFactory.getILoggerFactory();
-    }
-    
-    public ContextInitializer getContextInitializer() {
-    	ContextInitializer co = new ContextInitializer((LoggerContext) LoggerFactory.getILoggerFactory());
-    	return co;
-    }*/
     public String getLoggerImplementationLibraryName(){
     	StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
     	return binder.getLoggerFactoryClassStr();
@@ -59,9 +31,5 @@ public class LogstashLoggerFactory {
     
     public boolean isLogbackImplementationBinded(){
     	return getLoggerImplementationLibraryName().toLowerCase().contains("logback");
-    }
-    
-    public void writeLog() {
-    	getLogger().debug("HIHIHI");
     }
 }
--- LogstashUtility/src/test/java/com/ichatt/beagle/logstash/logger/LogstashLogFactoryWirteLogTest.java
+++ LogstashUtility/src/test/java/com/ichatt/beagle/logstash/logger/LogstashLogFactoryWirteLogTest.java
@@ -1,20 +1,31 @@
 package com.ichatt.beagle.logstash.logger;
 
 import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 import com.ichatt.beagle.logstash.logger.LogstashLoggerFactory;
 
 //import ch.qos.logback.core.joran.spi.JoranException;
 //import ch.qos.logback.core.util.StatusPrinter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class LogstashLogFactoryWirteLogTest {
 
 	@Test
 	public void logstashWriteTest() {
-		justCallTestMethod(0, 230);
-		justCallTestMethod(230, 230);
+		justCallTestMethod(0, 500);
 		try {
-			Thread.sleep(5000);
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+		//justCallTestMethod(230, 230);
+		try {
+			Thread.sleep(60000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -24,10 +35,10 @@ public class LogstashLogFactoryWirteLogTest {
 		//assertEquals(libnameStr, "org.slf4j.impl.Log4jLoggerFactory");
 		//assertEquals(libnameStr, "ch.qos.logback.classic.util.ContextSelectorStaticBinder");
 		//assertEquals(libnameStr, "org.apache.logging.slf4j.Log4jLoggerFactory");
-		boolean isLogbackImplBinded = LogstashLoggerFactory.getInstance().isLogbackImplementationBinded();
+		/*boolean isLogbackImplBinded = LogstashLoggerFactory.getInstance().isLogbackImplementationBinded();
 		assertEquals(true, isLogbackImplBinded);
 		boolean isLog4jImplBinded = LogstashLoggerFactory.getInstance().isLog4jImplementationBinded();
-		assertEquals(false, isLog4jImplBinded);
+		assertEquals(false, isLog4jImplBinded);*/
 		
 	}
 	
@@ -63,7 +74,67 @@ public class LogstashLogFactoryWirteLogTest {
 	
 	
 	public static void main(String[] args) {
-		new LogstashLogFactoryWirteLogTest().logstashWriteTest();
+//		new LogstashLogFactoryWirteLogTest().logstashWriteTest();
 		System.err.println("ERERERERERERER");
+//		
+		new LogstashLogFactoryWirteLogTest().new BasicThreadPoolExecutorExample().mains(null);
+	    
+		System.out.println("Done sir");
+		
+		try {
+            TimeUnit.SECONDS.sleep(60);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+	    
+	}
+	
+	class Task implements Runnable
+	{
+	    private String name;
+	 
+	    public Task(String name)
+	    {
+	        this.name = name;
+	    }
+	     
+	    public String getName() {
+	        return name;
+	    }
+	 
+	    @Override
+	    public void run()
+	    {
+	        try
+	        {
+	            Long duration = (long) (Math.random() * 10);
+	            System.out.println("Doing a task during : " + name);
+	            //TimeUnit.SECONDS.sleep(duration);
+	            new LogstashLogFactoryWirteLogTest().logstashWriteTest();
+	        }
+	        catch (/*InterruptedException e | */Exception e1)
+	        {
+	            e1.printStackTrace();
+	        }
+	    }
+	}
+	
+	public class BasicThreadPoolExecutorExample
+	{
+	    public void mains(String[] args)
+	    {
+	        //Use the executor created by the newCachedThreadPool() method
+	        //only when you have a reasonable number of threads
+	        //or when they have a short duration.
+	        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+	        for (int i = 0; i < 100; i++)
+	        {
+	            Task task = new Task("Task " + i);
+	            System.out.println("A new task has been added : " + task.getName());
+	            executor.execute(task);
+	        }
+	        //executor.shutdown();
+	        System.out.println("A new task has been added : " + "uhhhe");
+	    }
 	}
 }
